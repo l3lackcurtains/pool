1ff72713e128ad6eea15cfc1a017052fca1f497b
besoeasy <besoeasy@users.noreply.github.com>
2024-06-17 10:18:43 +0530

    chore: Update dependencies and add Gun library for distributed data storage

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ const { verifyCommit } = require("alberti-protocol-sdk");
 
 const difficulty = process.env.ALBERTI_DIFFICULTY || 4;
 
+const nodeurl = process.env.ALBERTI_NODEURL || "http://mynodeurl:3000";
+
 syncDatabase();
+
+const Gun = require("gun");
+
+const gun = Gun([
+  "http://localhost:8765/gun",
+  "https://gun-manhattan.herokuapp.com/gun",
+]);
 
 app.get("/commits/:offset", async (req, res) => {
   const offset = req.params.offset || 0;
@@ -83,6 +92,33 @@ app.post("/commit", async (req, res) => {
   }
 });
 
+let foundNodes = [];
+
+app.get("/nodes", async (req, res) => {
+  gun
+    .get("albertipools")
+    .map()
+    .once((data) => {
+      foundNodes.push({ node: data.node, difficulty: data.difficulty });
+    });
+
+  foundNodes = foundNodes.filter(
+    (v, i, a) => a.findIndex((t) => t.node === v.node) === i
+  );
+
+  res.json(foundNodes);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at ${port}`);
+  console.log(`Listening at ${port}`);
+
+  gun
+    .get("albertipools")
+    .set({ node: nodeurl, difficulty: difficulty }, (ack) => {
+      if (ack.err) {
+        console.error(ack.err);
+      } else {
+        console.log("Node URL Broadcasted......");
+      }
+    });
 });
